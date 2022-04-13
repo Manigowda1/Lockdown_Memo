c39ff721edc8bd8283bc60366cad9446e2d3ce43
Manikanta Gowda B <78212802+Manigowda1@users.noreply.github.com>
2022-04-13 19:42:08 +0530

    First commit

--- myproj/settings.py
+++ myproj/settings.py
@@ -129,6 +129,7 @@ MEDIA_ROOT = BASE_DIR
 
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
